42c60a2998e64631e1aea9fa36d450e87efd4815
Stephen St. Michael <stephenstmichael@gmail.com>
2023-02-08 06:52:22 -0600

    Adding git ignore and playing with text-based menu

--- code/inc/token.h
+++ code/inc/token.h
@@ -5,96 +5,113 @@
 	--
 */
 
-typedef enum TOKEN_TYPE {
+enum TOKEN {
 	
 	//punctuation
 	LPAREN,
 	RPAREN,
 	COLON,
-	SEMI_COLON,
-	COMMA,
-
+	SEMICOLON,
+	
 	//operators
-	ABS,
-	XNOR,
-	AND,
-	MOD,
-	NAND,
-	NOR,
-	NOT,
-	OR,
-	REM,
-	ROL,
-	ROR,
-	SLA,
-	SLL,
-	SRA,
-	SRL,
-	XOR,
-	EQUAL,
-	NEQUAL,
-	LESS_THAN,
-	LESS_THAN_EQ,
-	VAR_ASSIGN,
 	ASSIGN,
-	GREATER_THAN,
-	GREATER_THAN_EQ,
-	ADD,
-	SUB,
-	CONCAT,
-	MULT,
-	DIV,
-	EXP,
-
-	//data types
-	BIT,
-	BIT_VECTOR,
-	BOOLEAN,
-	INTEGER,
-	REAL,
-	TIME,
-	STD_LOGIC,
-	STD_LOGIC_VECTOR,
 	
-	//keywords:
-	ALL,
-	BEGIN,
-	CONST,
-	ELSE,
-	ELSIF,
-	END,
-	ENTITY,
-	IF,
-	IN,
-	INOUT,
-	IS,
-	LIBRARY,
-	MAP,
-	OF,
-	OUT,
-	PORT,
-	PROCESS,
-	RANGE,
-	TO,
-	SIGNAL,
-	USE,
-	VARIABLE,
-	WHEN,
-	
-	//literals
-	CHAR_LIT,
-	STR_LIT,
-	BIT_LIT,
-	NUM_LIT,
-	BASE_LIT,
-	PHY_LIT,
-
-	//other
-	IDENT,
-} ttype;
+};
 
 struct token {
-	ttype type;	
+	enum TOKEN type;	
 	char *literal;
 };
 
+enum VHD_TOKEN {
+	
+	//punctuation
+	VHD_LPAREN,
+	VHD_RPAREN,
+	VHD_COLON,
+	VHD_SEMI_COLON,
+	VHD_COMMA,
+
+	//operators
+	VHD_ABS,
+	VHD_XNOR,
+	VHD_AND,
+	VHD_MOD,
+	VHD_NAND,
+	VHD_NOR,
+	VHD_NOT,
+	VHD_OR,
+	VHD_REM,
+	VHD_ROL,
+	VHD_ROR,
+	VHD_SLA,
+	VHD_SLL,
+	VHD_SRA,
+	VHD_SRL,
+	VHD_XOR,
+	VHD_EQUAL,
+	VHD_NEQUAL,
+	VHD_LESS_THAN,
+	VHD_LESS_THAN_EQ,
+	VHD_VAR_ASSIGN,
+	VHD_ASSIGN,
+	VHD_GREATER_THAN,
+	VHD_GREATER_THAN_EQ,
+	VHD_ADD,
+	VHD_SUB,
+	VHD_CONCAT,
+	VHD_MULT,
+	VHD_DIV,
+	VHD_EXP,
+
+	//data types
+	VHD_BIT,
+	VHD_BIT_VECTOR,
+	VHD_BOOLEAN,
+	VHD_INTEGER,
+	VHD_REAL,
+	VHD_TIME,
+	VHD_STD_LOGIC,
+	VHD_STD_LOGIC_VECTOR,
+	
+	//keywords:
+	VHD_ALL,
+	VHD_BEGIN,
+	VHD_CONST,
+	VHD_ELSE,
+	VHD_ELSIF,
+	VHD_END,
+	VHD_ENTITY,
+	VHD_IF,
+	VHD_IN,
+	VHD_INOUT,
+	VHD_IS,
+	VHD_LIBRARY,
+	VHD_MAP,
+	VHD_OF,
+	VHD_OUT,
+	VHD_PORT,
+	VHD_PROCESS,
+	VHD_RANGE,
+	VHD_TO,
+	VHD_SIGNAL,
+	VHD_USE,
+	VHD_VARIABLE,
+	VHD_WHEN,
+	
+	//literals
+	VHD_CHAR_LIT,
+	VHD_STR_LIT,
+	VHD_BIT_LIT,
+	VHD_NUM_LIT,
+	VHD_BASE_LIT,
+	VHD_PHY_LIT,
+
+	//other
+	VHD_IDENT,
+};
+
+struct revToken {
+	enum VHD_TOKEN type;	
+	char *literal;
+};
--- code/main.c
+++ code/main.c
@@ -1,17 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #include "inc/lexer.h"
 
+void printWaterfall(void);
+void printOptions(void);
+
 int main(int argc, char* argv[]) {
 
-	printf("Hello, welcome to The VENT Transpiler!\n");
+	printWaterfall();
 
-	if(argc < 2){
-		printf("Usage: tvt adder.vhd\n");
+	char opt[256];
+	while(1){
+		printf("What would you like to do?\n");
+		printOptions();
+		
+		printf("\n=>");
+		fgets(opt, sizeof(opt), stdin);
+		
+		switch(opt[0]){
+		   
+			case 't':
+				printf("Transpiling...\n");
+				sleep(3);
+				break;
+		
+			case 'r':
+				printf("Reverse transpiling...\n");
+				sleep(3);
+				break;
+
+			case 'h':
+				printf("Help:\n");
+				break;
+			
+			case 'Q':
+			case 'q':
+				goto exit;
+		
+			default:
+				break;
+		}
+
+	}
+
+	if(argc < 3){
+		printf("Usage: tvt -t adder.vent\n");
 		return -1;
 	}	
 
-	FILE* vhdFile = fopen(argv[1], "r");
+	FILE* vhdFile = fopen(argv[2], "r");
 	
 	if(!vhdFile){
 		perror("Unable to open file!\n");
@@ -23,7 +62,44 @@ int main(int argc, char* argv[]) {
 	while(EOF != readChar(myLexer)){
 		printf("%c", getChar(myLexer));	
 	}
+	
+	free(myLexer);
 
-
+	exit:
+	
 	return 0;
+}
+
+
+void printWaterfall(void){
+	
+	const int waterFallDelay = 250000;
+
+	printf("-----------------------\n");
+	printf("The V E N T Transpiler\n");
+	usleep(waterFallDelay);
+	printf("     H x o e\n");
+	usleep(waterFallDelay);
+	printf("      D c t r\n");
+	usleep(waterFallDelay);
+	printf("       L e   r\n");
+	usleep(waterFallDelay);
+	printf("          p   i\n");
+	usleep(waterFallDelay);
+	printf("           t   b\n");
+	usleep(waterFallDelay);
+	printf("                l\n");
+	usleep(waterFallDelay);
+	printf("                 e\n");
+	usleep(waterFallDelay);
+	printf("-----------------------\n");
+}
+
+void printOptions(void){
+
+	printf(" t:   \tTranspile VENT to VHDL\n"
+		 	 " r:   \tReverse Transpile VHDL to VENT\n"
+			 " h:   \tHelp\n"
+			 " q/Q: \tQuit\n"); 
+
 }
