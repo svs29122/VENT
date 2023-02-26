1773fdc1b017619bd67557d00b4bb0ee3015afa4
svs29122 <stephenstmichael@yahoo.com>
2023-02-26 12:10:36 -0600

    Added more lexer tests

--- code/test/lexer_test.c
+++ code/test/lexer_test.c
@@ -5,7 +5,7 @@
 #include "cutest.h"
 #include "lexer.h"
 
-void TestNextToken(CuTest *tc){
+void TestNextToken_Single(CuTest *tc){
 	char* input = strdup("+");
 	struct lexer* tlex = NewLexer(input); 
 
@@ -18,11 +18,43 @@ void TestNextToken(CuTest *tc){
 	CuAssertStrEquals(tc, expLiteral, nt.literal); 
 	
 	free(tlex);
+	free(input);
+}
+
+void TestNextToken_Multiple(CuTest *tc){
+	char* input = strdup("():{},'/-+*=");
+	struct lexer* tlex = NewLexer(input); 
+
+	enum TOKEN_TYPE expToken[12] = {LPAREN,RPAREN,COLON, 
+											LBRACE,RBRACE,COMMA,
+											TICK,SLASH,MINUS,
+											PLUS,STAR,EQUAL};
+	
+	char expLiteral[12] = {'(', ')', ':', 
+								 '{', '}',',',
+								 '\'', '/', '-',
+								  '+', '*','='};
+
+	for(int i=0; i<12; i++){
+		Token nt = NextToken(tlex);
+	
+		char* eliteral = (char*) malloc(sizeof(char) * 2);
+		eliteral[0] = expLiteral[i];
+		eliteral[1] = '\0';
+
+		CuAssertIntEquals(tc, expToken[i], nt.type);
+		CuAssertStrEquals(tc, eliteral, nt.literal); 
+		
+		free(eliteral);
+	}
+
+	free(tlex);
 }
 
 CuSuite* LexerTestGetSuite(){
 	CuSuite* suite = CuSuiteNew();
-	SUITE_ADD_TEST(suite, TestNextToken);
+	SUITE_ADD_TEST(suite, TestNextToken_Single);
+	SUITE_ADD_TEST(suite, TestNextToken_Multiple);
 	return suite;
 }
 
