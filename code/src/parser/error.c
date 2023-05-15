57e86dcbc144c3487cb146a12d966f3b7703af6f
svs29122 <stephenstmichael@yahoo.com>
2023-05-15 09:09:32 -0500

    Cleaning up error handling in parser

--- code/src/parser/error.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-
-bool hadError;
-
-void error(int line, char* where, const char* message){
-	fprintf(stderr, "\e[0;31m[line %d] Error at \'%s\': %s\n\e[0m", line, where, message);
-	hadError = true;
-} 
