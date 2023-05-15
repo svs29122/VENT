35603d09d27b4df643a704a3cd495a4263331f4e
svs29122 <stephenstmichael@yahoo.com>
2023-05-15 09:09:32 -0500

    Cleaning up error handling in parser

--- code/src/parser/error.h
+++ code/src/parser/error.h
@@ -1,6 +1,12 @@
 #ifndef INC_ERROR_H
 #define INC_ERROR_H
 
-void error(int line, char* where, const char* message);
+bool hadError;
+
+void error(int line, char* where, const char* message){
+	fprintf(stderr, "\e[0;31m[line %d] Error at \'%s\': %s\n\e[0m", line, where, message);
+	hadError = true;
+}
+
 
 #endif //INC_ERROR_H
